3bc8dd8552f15af3abae8c02c3f87200eb0eb991
Haorong Yang <haorongy@seas.upenn.edu>
2020-11-27 01:26:55 -0500

    g2p

--- src/examples/computeBoids.ts
+++ src/examples/computeBoids.ts
@@ -19,11 +19,14 @@ export async function init(canvas: HTMLCanvasElement, useWGSL: boolean) {
   // Grid Parameters
   const minCorner = new Vector3(-1.0, -1.0, -1.0); // Min corner of the grid (also works as the origin of the grid for offsetting purposes)
   const maxCorner = new Vector3(1.0, 1.0, 1.0);  // Max corner of the grid
-  const h = 0.04; // Cell width of the grid
+  const h = 0.5; // Cell width of the grid
   const nxG = Math.floor((maxCorner.x - minCorner.x) / h) + 1;  // Number of grid points in the x-direction
   const nyG = Math.floor((maxCorner.y - minCorner.y) / h) + 1;  // Number of grid points in the y-direction
   const nzG = Math.floor((maxCorner.z - minCorner.z) / h) + 1;  // Number of grid points in the z-direction
   const numG = nxG * nyG * nzG; // Total number of grid points
+
+  // debug print
+  console.log(nxG, nyG, nzG);
 
   // Particle Attributes
   const E = 10000.0;  // Young's Modulus (Hardness)
@@ -40,7 +43,7 @@ export async function init(canvas: HTMLCanvasElement, useWGSL: boolean) {
   const rhoJello = 1000.0;  // Density of the points' material for jello
   const rhoSnow = 400;  // Density of the points' material for snow
   const rhoFluid = 997; // Density of the points' material for fluid
-  const numP = 1000;  // Total number of points
+  const numP = 4000;  // Total number of points
 
 
   // Calling navigator.gpu.requestAdapter() returns a JavaScript promise
@@ -130,17 +133,17 @@ export async function init(canvas: HTMLCanvasElement, useWGSL: boolean) {
   });
 
   /* -------------Example For Adding "Kernel"/Compute Shader (Part 1)------------------- */
-  // const computePipeline2 = device.createComputePipeline({
-  //   computeStage: {
-  //     module: 
-  //       // Deleted
-  //       device.createShaderModule({
-  //           code: glslShaders.compute2(numP),
-  //           transform: (glsl) => glslang.compileGLSL(glsl, "compute"),
-  //         }),
-  //     entryPoint: "main",
-  //   },
-  // });
+  const g2pPipeline = device.createComputePipeline({
+    computeStage: {
+      module: 
+        device.createShaderModule({
+            code: glslShaders.g2p(numP, numG),
+            transform: (glsl) => glslang.compileGLSL(glsl, "compute"),
+          }),
+      entryPoint: "main",
+    },
+  });
+  // --------------------------------------------------------------------------------------
 
 
   const depthTexture = device.createTexture({
@@ -445,13 +448,18 @@ export async function init(canvas: HTMLCanvasElement, useWGSL: boolean) {
       passEncoder.endPass();
     }
   /* -------------Example For Adding "Kernel"/Compute Shader (Part 2)------------------- */    
-    // {
-    //   // const passEncoder = commandEncoder.beginComputePass();
-    //   // passEncoder.setPipeline(computePipeline2);
-    //   // passEncoder.setBindGroup(0, bindGroup);
-    //   // passEncoder.dispatch(numP, numG);
-    //   // passEncoder.endPass();
-    // }
+    {
+      const passEncoder = commandEncoder.beginComputePass();
+      passEncoder.setPipeline(g2pPipeline);
+      passEncoder.setBindGroup(0, bindGroup);
+      let GPUGridDimX = 20000; // NOT THE MPM GRID
+      let GPUGridDimY = 1; // NOT THE MPM GRID
+      let GPUGridDimZ = 1; // NOT THE MPM GRID
+      passEncoder.dispatch(GPUGridDimX, GPUGridDimY, GPUGridDimZ);
+      passEncoder.endPass();
+    }
+  /* ----------------------------------------------------------------------------------- */     
+
     {
       const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
       passEncoder.setPipeline(renderPipeline);
@@ -489,7 +497,8 @@ void main() {
 layout(location = 0) in vec3 fs_pos;
 layout(location = 0) out vec4 fragColor;
 void main() {
-  fragColor = vec4((normalize(fs_pos) + vec3(1.0)) / 2.0, 1.0);
+  // fragColor = vec4((normalize(fs_pos) + vec3(1.0)) / 2.0, 1.0);
+  fragColor = vec4(1,1,1,0);
 }`,
 
   compute: (numPArg: number, numGArg: number) => `#version 450
@@ -575,54 +584,168 @@ void main() {
   vec3 dY = gridNodes.data[30].v * 0.1;
   float dY2 = gridNodes.data[30].m * 0.1;
   vec3 dY3 = particles2.data[index].C[2] * 0.01;
-  particles1.data[index].pos += vec4(dY3, 0);
+  vec3 testF = particles2.data[index].F[0] * 0.00001;
+  // particles1.data[index].pos += vec4(0, 0, 0, 0);
 }`,
 
   /* -------------Example For Adding "Kernel"/Compute Shader (Part 3)------------------- */
-// compute2: (numPArg: number, numGArg: number) => `#version 450
-// struct Particle {
-//   vec4 pos;
-//   vec4 vel;
-// };
+g2p: (numPArg: number, numGArg: number) => `#version 450
+layout(std140, set = 0, binding = 0) uniform SimParams {
+  float dt; // Timestep
+  float gravityX;  // Gravity (x-component)
+  float gravityY;  // Gravity (y-component)
+  float gravityZ;  // Gravity (z-component)
+  float minCornerX; // Min corner of the grid (x-component) (also works as the origin of the grid for offsetting purposes)
+  float minCornerY; // Min corner of the grid (y-component) (also works as the origin of the grid for offsetting purposes)
+  float minCornerZ; // Min corner of the grid (z-component) (also works as the origin of the grid for offsetting purposes)
+  float maxCornerX;  // Max corner of the grid (x-component)
+  float maxCornerY;  // Max corner of the grid (y-component)
+  float maxCornerZ;  // Max corner of the grid (z-component)
+  float h; // Cell width of the grid
+  float nxG;  // Number of grid points in the x-direction
+  float nyG;  // Number of grid points in the y-direction
+  float nzG;  // Number of grid points in the z-direction
+  float numG; // Total number of grid points
+  float E;  // Young's Modulus (Hardness)
+  float E0; // Initial Young's Modulus (for snow)
+  float nu; // Poisson's Ratio (Incompressibility)
+  float nuSnow; // Poisson's Ratio (for snow)
+  float thetaC; // Critical compression (for snow)
+  float thetaS;  // Critical stretch (for snow)
+  float xi;  // Hardening coefficient (for snow)
+  float mu;  // One of the Lamé parameters
+  float lambda;  // One of the Lamé parameters
+  float lambdaFluid; // parameter for fluid
+  float gamma;  // parameter for fluid
+  float rhoJello;  // Density of the points' material for jello
+  float rhoSnow;  // Density of the points' material for snow
+  float rhoFluid; // Density of the points' material for fluid
+  float numP;  // Total number of points
+  float PADDING_1; // IGNORE
+  float PADDING_2; // IGNORE
+} params;
 
-// layout(std140, set = 0, binding = 0) uniform SimParams {
-//   float deltaT;
-//   float rule1Distance;
-//   float rule2Distance;
-//   float rule3Distance;
-//   float rule1Scale;
-//   float rule2Scale;
-//   float rule3Scale;
-// } params;
+struct ParticleStruct1 {
+  vec4 pos; // (pos.xyz => Particle Position, pos.w => Particle Material Type)
+  vec4 v; // (v.xyz => Particle Velocity, v.w => Particle Mass)
+};
 
-// layout(std140, set = 0, binding = 1) buffer ParticlesA {
-//   Particle particles[${numPArg}];
-// } particlesA;
+struct ParticleStruct2 {
+  mat3 F; // Deformation Graident Of The Particle
+  mat3 Fe;  // Elastic Component Of The Deformation Gradient Of The Particle
+  mat3 Fp;  // Plastic Component Of The Deformation Gradient Of The Particle
+  mat3 C; // APIC's C Matrix Of The Particle
+  float J;  // J attribute Of The Particle
+  float vol;  // Volume Of The Particle
+  float PADDING_1;  // (IGNORE)
+  float PADDING_2;  // (IGNORE)
+};
 
-// layout(std140, set = 0, binding = 2) buffer ParticlesB {
-//   Particle particles[${numPArg}];
-// } particlesB;
+struct GridNodeStruct {
+  vec3 vN;  // New Velocity Stored On The Grid Node
+  vec3 v; // Old Velocity Stored On The Grid Node
+  vec3 force; // Force Stored On The Grid Node
+  float m;  // Mass Stored On The Grid Node
+  float PADDING_1;  // (IGNORE)
+  float PADDING_2;  // (IGNORE)
+  float PADDING_3;  // (IGNORE)
+};
 
-// void main() {
-//   // https://github.com/austinEng/Project6-Vulkan-Flocking/blob/master/data/shaders/computeparticles/particle.comp
+layout(std430, set = 0, binding = 1) buffer PARTICLES1 {
+  ParticleStruct1 data[${numPArg}];
+} particles1;
 
-//   uint index = gl_GlobalInvocationID.x;
-//   if (index >= ${numPArg}) { return; }
+layout(std430, set = 0, binding = 2) buffer PARTICLES2 {
+  ParticleStruct2 data[${numPArg}];
+} particles2;
 
-//   vec3 vPos = particlesB.particles[index].pos.xyz;
-//   vec3 vVel = particlesB.particles[index].vel.xyz;
+layout(std430, set = 0, binding = 3) buffer GRIDNODES {
+  GridNodeStruct data[${numGArg}];
+} gridNodes;
 
-//   // Wrap around boundary
-//   if (vPos.x < -1.0) vPos.x = 1.0;
-//   if (vPos.x > 1.0) vPos.x = -1.0;
-//   if (vPos.y < -1.0) vPos.y = 1.0;
-//   if (vPos.y > 1.0) vPos.y = -1.0;
-//   if (vPos.z < -1.0) vPos.z = 1.0;
-//   if (vPos.z > 1.0) vPos.z = -1.0;
 
-//   particlesB.particles[index].pos = vec4(vPos,1);
+// Compute weights (when each thread handles a particle)
+void computeWeights1D_P(float x, out vec3 w, out vec3 dw, out int baseNode) {
+  // x is the particle's index-space position and can represent particle's index-space position in x, y, or z direction,
+  // baseNode can also represent the baseNode in x, y, or z direction, depending on how this function is used
+  // Note that here we compute the 1D quadratic B spline weights and
+  // x is assumed to be scaled in the index space (in other words, the grid has cell width of length 1)
+  baseNode = int(floor(x - 0.5));
+  float d0 = x - baseNode;
+  w[0] = 0.5 * (1.5 - d0) * (1.5 - d0);
+  dw[0] = d0 - 1.5;
+  float d1 = x - (baseNode + 1);
+  w[1] = 0.75 - d1 * d1;
+  dw[1] = -2 * d1;
+  float d2 = x - (baseNode + 2);
+  w[2] = 0.5 * (1.5 + d2) * (1.5 + d2);
+  dw[2] = 1.5 + d2;
+}
 
-//   // Write back
-//   particlesB.particles[index].vel = vec4(vVel,1);
-// }`,
+
+int coordinateToId(ivec3 c) {
+  return c[0] + int(params.nxG) * c[1] + int(params.nxG) * int(params.nyG) * c[2];
+}
+
+void main() {
+  uint index = gl_GlobalInvocationID.x;
+  if (index >= ${numPArg}) { return; }
+
+  // vec3 dY = gridNodes.data[30].v * 0.1;
+  // float dY2 = gridNodes.data[30].m * 0.1;
+  // vec3 dY3 = particles2.data[index].C[2] * 0.01;
+
+
+  // -----------------------------------------
+  // loop through the nearby 3*3*3 grids and gather the information to the particles
+
+  vec3 minCorner = vec3(params.minCornerX, params.minCornerY, params.minCornerZ);
+  vec3 posP_index_space = (particles1.data[index].pos.xyz - minCorner) / params.h;
+  vec3 wI, wJ, wK;
+  vec3 dwI, dwJ, dwK;
+  int baseNodeI, baseNodeJ, baseNodeK;
+
+  computeWeights1D_P(posP_index_space.x, wI, dwI, baseNodeI);
+  computeWeights1D_P(posP_index_space.y, wJ, dwJ, baseNodeJ);
+  computeWeights1D_P(posP_index_space.z, wK, dwK, baseNodeK);
+
+  vec3 vP_PIC = vec3(0.0);
+  vec3 vP_FLIP = particles1.data[index].v.xyz;
+  mat3 CMat = mat3(0.0);
+  
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+        for (int k = 0; k < 3; k++) {
+            int nodeI = baseNodeI + i;
+            int nodeJ = baseNodeJ + j;
+            int nodeK = baseNodeK + k;
+
+            int nodeID = coordinateToId(ivec3(nodeI, nodeJ, nodeK));
+            float weightIJK = wI[i] * wJ[j] * wK[k];
+            
+            vP_PIC += gridNodes.data[nodeID].vN * weightIJK;
+
+            // ignore APIC at the moment
+            vec3 posG = vec3(float(nodeI), float(nodeJ), float(nodeK)) * params.h + minCorner;
+            vec3 a = gridNodes.data[nodeID].vN;
+            vec3 b = posG - particles1.data[index].pos.xyz;
+            mat3 ab = mat3(vec3(a.x*b.x, a.y*b.x, a.z*b.x), vec3(a.x*b.y, a.y*b.y, a.z*b.y), vec3(a.x*b.z, a.y*b.z, a.z*b.z));
+            CMat += (weightIJK * 4.0 / (params.h * params.h)) * ab;
+        }
+    }
+  }
+  
+  // ignore apic for now
+  particles1.data[index].v.xyz = vP_PIC.xyz;
+  particles2.data[index].C = CMat;
+  particles1.data[index].pos.xyz += params.dt * vP_PIC.xyz;
+   
+
+  vec3 testF = particles2.data[index].F[0] * 0.0;
+  particles1.data[index].pos += vec4(testF, 0);
+
+
+}`,
+// --------------------------------------------------------------------------------
+
 };
