c54c4a909c3d563d8767b8a4c9ab3a1878dbda43
Jacky Lu <jackylu0124@gmail.com>
2020-12-06 14:45:51 -0500

    Fixed snow and set up jello, snow, and fluid coupling scene

--- src/shaders/rendering.ts
+++ src/shaders/rendering.ts
@@ -7,16 +7,33 @@ export const renderingShaders = {
     
   layout(location = 0) in vec4 a_particlePos;
   layout(location = 1) in vec4 a_particleVel;
-  layout(location = 0) out vec3 fs_pos;
+  layout(location = 0) out vec4 fs_pos;
+  layout(location = 1) out vec4 fs_vel;
   void main() {
     gl_Position = uniforms.modelViewProjectionMatrix * vec4(vec3(a_particlePos), 1.0);
-    fs_pos = a_particlePos.xyz;
+    fs_pos = a_particlePos;
+    fs_vel = a_particleVel;
   }`,
   
     fragment: `#version 450
-  layout(location = 0) in vec3 fs_pos;
+  layout(location = 0) in vec4 fs_pos;
+  layout(location = 1) in vec4 fs_vel;
   layout(location = 0) out vec4 fragColor;
   void main() {
-    fragColor = vec4(1,1,1,1);
+    
+    if (abs(fs_pos.w - 0) <= 0.1) {
+      // JELLO
+      fragColor = vec4(0,1,0,1);
+    } else if (abs(fs_pos.w - 1) <= 0.1) {
+      // SNOW
+      fragColor = vec4(1,0,0,1);
+    } else if (abs(fs_pos.w - 2) <= 0.1) {
+      // FLUID
+      fragColor = vec4(0,0,1,1);
+    } else {
+      // SHOULD NOT GET TO THIS ELSE STATEMENT IF EVERYTHING WORKS RIGHT
+      fragColor = vec4(1,1,1,1);
+    }
+
   }`,
 };
--- src/utilities/simulationParameters.ts
+++ src/utilities/simulationParameters.ts
@@ -28,7 +28,11 @@ export const gamma = 7;  // parameter for fluid
 export const rhoJello = 1000.0;  // Density of the points' material for jello
 export const rhoSnow = 400;  // Density of the points' material for snow
 export const rhoFluid = 997; // Density of the points' material for fluid
-export const numP = 128000; // 4096 * 2; // Total number of points
+
+export const numPJello = 16 * 16 * 16 * 8;
+export const numPSnow = 16 * 16 * 16 * 8;
+export const numPFluid = 128000;  // 128000
+export const numP = numPJello + numPSnow + numPFluid; // 4096 * 2; // Total number of points
 
 export const simParamData = new Float32Array([
     dt, // Timestep
@@ -94,34 +98,53 @@ export const simParamData = new Float32Array([
 
   let matIdentity : number[] = [1, 0, 0, 0,/*Col 1*/ 0, 1, 0, 0,/*Col 2*/ 0, 0, 1, 0/*Col 3*/];
   let volumeP = h * h * h / 8.0;
+  let massJello = volumeP * rhoJello;
+  let massSnow = volumeP * rhoSnow;
+  let massFluid = volumeP * rhoFluid;
+
+  let counterJello = 0;
+  let counterSnow =0;
+  let counterFluid = 0;
   for (let i = 0; i < numP; i++) {
-    // Fill in p1Data
-    let matType = 2;
-    let mass = 0;
-    switch (matType) {
-      case 0:
-        mass = volumeP * rhoJello;
-        break;
-      case 1:
-        mass = volumeP * rhoSnow;
-        break;
-      case 2:
-        mass = volumeP * rhoFluid;
-        break;  
+    
+    if (i >= 0 && i < numPJello) {
+      // Jello Block (Bottom Block)
+      p1Data[8 * i + 0] = Math.random() * (16 * h) - 0 * h; // Particle Position X Component (1 float)
+      p1Data[8 * i + 1] = Math.random() * (16 * h) + 5 * h - 1; // Particle Position Y Component (1 float)
+      p1Data[8 * i + 2] = Math.random() * (16 * h) - 8 * h; // Particle Position Z Component (1 float)
+      p1Data[8 * i + 3] = 0;  // Particle Material Type (1 float)
+      p1Data[8 * i + 4] = 0;  // Particle Velocity X Component (1 float)
+      p1Data[8 * i + 5] = 0;  // Particle Velocity Y Component (1 float)
+      p1Data[8 * i + 6] = 0;  // Particle Velocity Z Component (1 float)
+      p1Data[8 * i + 7] = massJello; // Particle Mass (1 float)  
+
+      counterJello++;
+    } else if (i < numPJello + numPSnow){
+      // Snow Block (Top Block)
+      p1Data[8 * i + 0] = Math.random() * (16 * h) - 8 * h + 14 * h; // Particle Position X Component (1 float)
+      p1Data[8 * i + 1] = Math.random() * (16 * h) + ((nyG - 1) / 2 - 16 - 3) * h; // Particle Position Y Component (1 float)
+      p1Data[8 * i + 2] = Math.random() * (16 * h) - 8 * h + 4 * h; // Particle Position Z Component (1 float)
+      p1Data[8 * i + 3] = 1;  // Particle Material Type (1 float)
+      p1Data[8 * i + 4] = 0;  // Particle Velocity X Component (1 float)
+      p1Data[8 * i + 5] = 0;  // Particle Velocity Y Component (1 float)
+      p1Data[8 * i + 6] = 0;  // Particle Velocity Z Component (1 float)
+      p1Data[8 * i + 7] = massSnow; // Particle Mass (1 float)
+
+      counterSnow++;
+    } else {
+      // 128k Fluid
+      p1Data[8 * i + 0] = Math.random() * (10 * h) + 5 * h - 1; // Particle Position X Component (1 float)
+      p1Data[8 * i + 1] = Math.random() * (40 * h) + 5 * h - 1; // Particle Position Y Component (1 float)
+      p1Data[8 * i + 2] = Math.random() * (40 * h) + 4 * h - 1; // Particle Position Z Component (1 float)
+      p1Data[8 * i + 3] = 2;  // Particle Material Type (1 float)
+      p1Data[8 * i + 4] = 0;  // Particle Velocity X Component (1 float)
+      p1Data[8 * i + 5] = 0;  // Particle Velocity Y Component (1 float)
+      p1Data[8 * i + 6] = 0;  // Particle Velocity Z Component (1 float)
+      p1Data[8 * i + 7] = massFluid; // Particle Mass (1 float)
+
+      counterFluid++;
     }
 
-    // p1Data[8 * i + 0] = Math.random() * (8 * h) - 4 * h; // Particle Position X Component (1 float)
-    // p1Data[8 * i + 1] = Math.random() * (16 * h) + (0.5 - 8 * h); // Particle Position Y Component (1 float)
-    // p1Data[8 * i + 2] = Math.random() * (8 * h) - 4 * h; // Particle Position Z Component (1 float)
-
-    p1Data[8 * i + 0] = Math.random() * (10 * h) + 5 * h - 1; // Particle Position X Component (1 float)
-    p1Data[8 * i + 1] = Math.random() * (40 * h) + 5 * h - 1; // Particle Position Y Component (1 float)
-    p1Data[8 * i + 2] = Math.random() * (40 * h) + 4 * h - 1; // Particle Position Z Component (1 float)
-    p1Data[8 * i + 3] = matType;  // Particle Material Type (1 float)
-    p1Data[8 * i + 4] = 0;  // Particle Velocity X Component (1 float)
-    p1Data[8 * i + 5] = 0;  // Particle Velocity Y Component (1 float)
-    p1Data[8 * i + 6] = 0;  // Particle Velocity Z Component (1 float)
-    p1Data[8 * i + 7] = mass; // Particle Mass (1 float)
 
     // Fill in p2Data
     for (let matrixIndex = 0; matrixIndex < 12; matrixIndex++) {
@@ -156,3 +179,8 @@ export const simParamData = new Float32Array([
     gData[16 * i + 14] = 0;  // PADDING (1 float)
     gData[16 * i + 15] = 0;  // PADDING (1 float)
   }
+
+  console.log("Number Of Jello Particles: " + counterJello.toString());
+  console.log("Number Of Snow Particles: " + counterSnow.toString());
+  console.log("Number Of Fluid Particles: " + counterFluid.toString());
+  console.log("Total Number Of Particles: " + (counterJello + counterSnow + counterFluid).toString());
